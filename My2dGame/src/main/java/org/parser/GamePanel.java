8a7dc8d20a7d4318cf5095fbed1c7f676d684978
JanCiepiela <jan.ciepiela@sovva.ai>
2025-08-19 18:13:49 +0200

    setup game loop with initial white character object

--- My2dGame/src/main/java/org/parser/GamePanel.java
+++ My2dGame/src/main/java/org/parser/GamePanel.java
@@ -30,7 +30,27 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
 
-        //tbc game loop here
+        while(gameThread != null) {
+            //In game loop:
+            //1.UPDATE - We update information such as character position
+            update();
 
+            //2.DRAW - We draw screen with the updated information
+            repaint();
+
+            //If FPS is 30, the program updates and draws 30 times per second
+        }
     }
+
+    public void update() {}
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.white);
+        g2.fillRect(100,100,tileSize,tileSize);
+        g2.dispose();
+    }
+
 }
